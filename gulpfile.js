5dd8ab87abfb7070557bb5d9e57fd7d25d7135f8
Dana Ng <ngdana1@gmail.com>
2017-07-07 18:58:31 -0700

    updated project with gulp minification of html, js, css and images - desktop score 91 and mobile 77

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,111 @@
 var gulp = require('gulp');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var stripedebug = require('gulp-strip-debug');
+var deporder = require('gulp-deporder');
+var htmlmin = require('gulp-htmlmin');
+var imagemin = require('gulp-imagemin');
+
+// development mode?
+devBuild = (process.env.NODE_ENV !== 'production');
+
+// folders
+folder = {
+  main: './',
+  src: 'src/',
+  build: 'dist/'
+};
 
 
+/** VIEWS FOLDER MINIFICATION **/
 
-gulp.task('default', function() {
+// html minification
+gulp.task('viewsminify', function() {
+  return gulp.src('src/views/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest(folder.build + 'views'));
+});
+
+// js files minified
+gulp.task('viewsjs', function() {
+  var jsbuild = gulp.src(folder.src + 'views/js/*')
+    .pipe(deporder());
+
+    if(!devBuild) {
+      jsbuild = jsbuild
+        .pipe(stripedebug())
+        .pipe(uglify());
+    }
+    return jsbuild.pipe(gulp.dest(folder.build + 'views/js/'));
+});
+
+// minify view images
+gulp.task('viewsimageminify', function() {
+  gulp.src('views/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest(folder.build + 'views/images/'))
+});
+
+/*** REGULAR FILES ***/
+
+// Other HTML files minified:
+gulp.task('minify', function() {
+  return gulp.src(folder.src + '/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('js', function() {
+  var jsbuild = gulp.src(folder.src + 'js/')
+    .pipe(deporder());
+
+    if(!devBuild) {
+      jsbuild = jsbuild
+        .pipe(stripedebug())
+        .pipe(uglify());
+    }
+    return jsbuild.pipe(gulp.dest(folder.build + 'js/'));
+});
+
+// minify and rename css with suffix min
+gulp.task('css', function() {
 	return gulp.src('./css/*.css') // src files
 		.pipe(cssmin())
 		.pipe(rename({suffix: '.min'})) // gulp plugin
-		.pipe(gulp.dest('dist/css/')) // output
+		.pipe(gulp.dest(folder.build + 'css/')) // output
 });
+
+// minify images
+
+gulp.task('imageminify', function() {
+  gulp.src('/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest( folder.build + '/images'))
+});
+
+
+/** RUN AND WATCH TASKS ***/
+
+// run all tasks
+gulp.task('run', ['css', 'js', 'minify', 'viewsminify', 'viewsjs', 'viewsimageminify', 'imageminify']);
+
+// watch for changes
+gulp.task('watch', function() {
+
+  // views changes:
+  gulp.watch(folder.src + 'views/**/*', ['viewsminify', 'viewsjs', 'viewsimageminify']);
+
+
+  // regular changes:
+  gulp.watch(folder.src + '*.html', ['minify']);
+  gulp.watch(folder.src + 'js/**/*', ['js']);
+  gulp.watch(folder.src + 'css/**/*', ['css']);
+  gulp.watch(folder.src + 'img/**/*', ['imageminify']);
+
+});
+
+
+// default task
+gulp.task('default', ['run', 'watch']);
+
