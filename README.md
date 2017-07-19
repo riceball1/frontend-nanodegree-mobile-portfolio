6dd1e11cc58a308a0ac057134acc3c6b7c704e7e
Dana Ng <ngdana1@gmail.com>
2017-07-19 12:41:58 -0700

    updated with gulpfile that minifies images

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 ## Website Performance Optimization portfolio project
 
+
+## Optimizations
+
+The following are a list of optimizations that I did in order to get a 90 or above speed score for the website:
+
+- Create a gulpfile.js that minifies html, css, and images
+
+
+
+
+
+## Description of Project From Udacity
+
 Your challenge, if you wish to accept it (and we sure hope you will), is to optimize this online portfolio for speed! In particular, optimize the critical rendering path and make this page render as quickly as possible by applying the techniques you've picked up in the [Critical Rendering Path course](https://www.udacity.com/course/ud884).
 
 To get started, check out the repository and inspect the code.
--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,9 @@ devBuild = (process.env.NODE_ENV !== 'production');
 
 // folders
 folder = {
-  main: './',
-  src: 'src/',
-  build: 'dist/'
+    main: './',
+    src: 'src/',
+    build: 'dist/'
 };
 
 
@@ -22,99 +22,102 @@ folder = {
 
 // html minification
 gulp.task('viewsminify', function() {
-  return gulp.src('src/views/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest(folder.build + 'views'));
+    return gulp.src('src/views/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest(folder.build + 'views'));
 });
 
 // minify css
 // minify and rename css with suffix min
 gulp.task('viewscss', function() {
-  return gulp.src(folder.src + 'views/css/*.css') // src files
-    .pipe(cssmin())
-    .pipe(rename({suffix: '.min'})) // gulp plugin
-    .pipe(gulp.dest(folder.build + 'views/css/')) // output
+    return gulp.src(folder.src + 'views/css/*.css') // src files
+        .pipe(cssmin())
+        .pipe(rename({ suffix: '.min' })) // gulp plugin
+        .pipe(gulp.dest(folder.build + 'views/css/')) // output
 });
 
 // js files minified
 gulp.task('viewsjs', function() {
-  var jsbuild = gulp.src(folder.src + 'views/js/*')
-    .pipe(deporder());
+    var jsbuild = gulp.src(folder.src + 'views/js/*')
+        .pipe(deporder());
 
-    if(!devBuild) {
-      jsbuild = jsbuild
-        .pipe(stripedebug())
-        .pipe(uglify());
+    if (!devBuild) {
+        jsbuild = jsbuild
+            .pipe(stripedebug())
+            .pipe(uglify());
     }
     return jsbuild.pipe(gulp.dest(folder.build + 'views/js/'));
 });
 
 // minify view images
 gulp.task('viewsimageminify', function() {
-  gulp.src(folder.src + 'views/images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest(folder.build + 'views/images/'))
-});
+            gulp.src(folder.src + 'views/images/*')
+                .pipe(imagemin({
+                        verbose: true
+                    }))
+                    .pipe(gulp.dest(folder.build + 'views/images/'))
+                });
 
-/*** REGULAR FILES ***/
+        /*** REGULAR FILES ***/
 
-// Other HTML files minified:
-gulp.task('minify', function() {
-  return gulp.src(folder.src + '/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('./'));
-});
+        // Other HTML files minified:
+        gulp.task('minify', function() {
+            return gulp.src(folder.src + '/*.html')
+                .pipe(htmlmin({ collapseWhitespace: true }))
+                .pipe(gulp.dest('./'));
+        });
 
-gulp.task('js', function() {
-  var jsbuild = gulp.src(folder.src + 'js/')
-    .pipe(deporder());
+        gulp.task('js', function() {
+            var jsbuild = gulp.src(folder.src + 'js/')
+                .pipe(deporder());
 
-    if(!devBuild) {
-      jsbuild = jsbuild
-        .pipe(stripedebug())
-        .pipe(uglify());
-    }
-    return jsbuild.pipe(gulp.dest(folder.build + 'js/'));
-});
+            if (!devBuild) {
+                jsbuild = jsbuild
+                    .pipe(stripedebug())
+                    .pipe(uglify());
+            }
+            return jsbuild.pipe(gulp.dest(folder.build + 'js/'));
+        });
 
-// minify and rename css with suffix min
-gulp.task('css', function() {
-	return gulp.src('./css/*.css') // src files
-		.pipe(cssmin())
-		.pipe(rename({suffix: '.min'})) // gulp plugin
-		.pipe(gulp.dest(folder.build + 'css/')) // output
-});
+        // minify and rename css with suffix min
+        gulp.task('css', function() {
+            return gulp.src('./css/*.css') // src files
+                .pipe(cssmin())
+                .pipe(rename({ suffix: '.min' })) // gulp plugin
+                .pipe(gulp.dest(folder.build + 'css/')) // output
+        });
 
-// minify images
+        // minify images
 
-gulp.task('imageminify', function() {
-  gulp.src('./img/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest( folder.build + '/img'))
-});
-
-
-/** RUN AND WATCH TASKS ***/
-
-// run all tasks
-gulp.task('run', ['css', 'js', 'minify', 'viewsminify', 'viewsjs', 'viewsimageminify', 'imageminify', 'viewscss']);
-
-// watch for changes
-gulp.task('watch', function() {
-
-  // views changes:
-  gulp.watch(folder.src + 'views/**/*', ['viewsminify', 'viewsjs', 'viewsimageminify', 'viewscss']);
+        gulp.task('imageminify', function() {
+            gulp.src(folder.src + 'img/*')
+                .pipe(imagemin({
+                    verbose: true
+                }))
+                .pipe(gulp.dest(folder.build + 'img/'))
+        });
 
 
-  // regular changes:
-  gulp.watch(folder.src + '*.html', ['minify']);
-  gulp.watch(folder.src + 'js/**/*', ['js']);
-  gulp.watch(folder.src + 'css/**/*', ['css']);
-  gulp.watch(folder.src + 'img/**/*', ['imageminify']);
+        /** RUN AND WATCH TASKS ***/
 
-});
+        // run all tasks
+        gulp.task('run', ['css', 'js', 'minify', 'viewsminify', 'viewsjs', 'viewsimageminify', 'imageminify', 'viewscss']);
+
+        // watch for changes
+        gulp.task('watch', function() {
+
+            // views changes:
+            gulp.watch(folder.src + 'views/**/*', ['viewsminify', 'viewsjs', 'viewsimageminify', 'viewscss']);
 
 
-// default task
-gulp.task('default', ['run', 'watch']);
+            // regular changes:
+            gulp.watch(folder.src + '*.html', ['minify']);
+            gulp.watch(folder.src + 'js/**/*', ['js']);
+            gulp.watch(folder.src + 'css/**/*', ['css']);
+            gulp.watch(folder.src + 'img/**/*', ['imageminify']);
 
+        });
+
+
+        // default task
+        gulp.task('default', ['run', 'watch']);
