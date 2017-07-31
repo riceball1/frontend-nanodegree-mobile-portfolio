5115c5a3f725823e90a561242fc34f528aef9d6b
Dana Ng <ngdana1@gmail.com>
2017-07-30 22:41:37 -0700

    fixed sources of pizza images

--- views/js/main.js
+++ views/js/main.js
@@ -399,7 +399,7 @@ var pizzaElementGenerator = function(i) {
     pizzaContainer.id = "pizza" + i; // gives each pizza element a unique id
     pizzaImageContainer.style.width = "35%";
 
-    pizzaImage.src = "images/pizza.png";
+    pizzaImage.src = "images/pizza.min.png"; // changed to minified pizza
     pizzaImage.classList.add("img-responsive");
     pizzaImageContainer.appendChild(pizzaImage);
     pizzaContainer.appendChild(pizzaImageContainer);
@@ -566,7 +566,7 @@ document.addEventListener('DOMContentLoaded', function() {
     for (var i = 0; i < pizzaAmount; i++) {
         var elem = document.createElement('img');
         elem.className = 'mover';
-        elem.src = "images/pizza-min.png";
+        elem.src = "images/pizza.min.png";
         elem.style.height = "100px";
         elem.style.width = "73.333px";
         elem.basicLeft = (i % cols) * s;
